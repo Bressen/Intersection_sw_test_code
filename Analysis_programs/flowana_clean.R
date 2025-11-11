c83210b9daa9857947e1e11a3bba7971452462e1
Pipper <cbpi@novonordisk.com>
2025-11-11 14:55:02 +0100

    Scripts for paper

--- Analysis_programs/flowana_clean.R
+++ Analysis_programs/flowana_clean.R
@@ -15,37 +15,54 @@ thetahat<-c(0.0315,2.681,3.153)
 #Estimated variance######################################
 #########################################################
 
-Sigmahat<-matrix(c(0.403,  -1.560,  4.480,
+Sigmahat <- matrix(c(0.403,  -1.560,  4.480,
                   -1.560, 846.241, 822.862,
                   4.480, 822.862, 890.064),3,3)
 
-vcov.thetahat<-Sigmahat/3532 #3532 out of 3533 randomized subjects included in analysis. 
+vcov.thetahat <- Sigmahat/3532 #3532 out of 3533 randomized subjects included in analysis. 
 
 #########################################################
-#P-values: signed wald test##############################
+#Creating estimate object################################
 #########################################################
 
-data.frame(pvals123=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=rep(1,3)/3)$p.value,
-pvals12=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=rep(1,3)/3,index=c(1,2))$p.value,
-pvals13=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=rep(1,3)/3,index=c(1,3))$p.value,
-pvals23=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=rep(1,3)/3,index=c(2,3))$p.value,
-pvals123.1=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=c(0.2,0.4,0.4))$p.value,
-pvals12.1=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=c(0.2,0.4,0.4),index=c(1,2))$p.value,
-pvals13.1=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=c(0.2,0.4,0.4),index=c(1,3))$p.value,
-pvals1=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=rep(1,3)/3,index=c(1))$p.value,
-pvals2=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=rep(1,3)/3,index=c(2))$p.value,
-pvals3=targeted::test_intersection_sw(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),weights=rep(1,3)/3,index=c(3))$p.value)
+est_obj <- lava::estimate(coef=thetahat,vcov=vcov.thetahat)
+
+#########################################################
+#Signed wald test equal weights #########################
+#########################################################
+
+sw_equalweights <- lava::closed_testing(
+  est_obj,
+  test = targeted::test_intersection_sw,
+  noninf = rep(0, 3),
+  weights = rep(1, 3)/3,
+  nsim.null=100000
+)
+sw_equalweights$p.value
+summary(sw_equalweights)
+
+#########################################################
+#Signed wald test upweighted H2,H3 ######################
+#########################################################
+
+sw_upweighted <- lava::closed_testing(
+  est_obj,
+  test = targeted::test_intersection_sw,
+  noninf = rep(0, 3),
+  weights = c(0.2,0.4,0.4),
+  nsim.null=100000
+)
+sw_upweighted$p.value
+summary(sw_upweighted)
 
 #########################################################
 #P-values: Minimum p-value test##########################
 #########################################################
 
-
-data.frame(pvals123=targeted::test_zmax_onesided(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3))$p.value,
-           pvals12=targeted::test_zmax_onesided(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),index=c(1,2))$p.value,
-           pvals13=targeted::test_zmax_onesided(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),index=c(1,3))$p.value,
-           pvals23=targeted::test_zmax_onesided(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),index=c(2,3))$p.value,
-           pvals1=targeted::test_zmax_onesided(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),index=c(1))$p.value,
-           pvals2=targeted::test_zmax_onesided(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),index=c(2))$p.value,
-           pvals3=targeted::test_zmax_onesided(par=thetahat,vcov=vcov.thetahat,noninf=rep(0,3),index=c(3))$p.value)
-
+p_min <- lava::closed_testing(
+  est_obj,
+  test = targeted::test_zmax_onesided,
+  noninf = rep(0, 3)
+)
+p_min$p.value
+summary(p_min)
